a049776d099a71a9881f0bb83c0fe6a9a5e1a2ea
Victor Costan <costan@gmail.com>
2011-03-12 04:06:30 -0500

    Gravatars in newsfeed.

---
+++ test/unit/helpers/feed_items_helper_test.rb
@@ -0,0 +1,12 @@
+require 'test_helper'
+
+class FeedItemsHelperTest < ActionView::TestCase
+  test 'gravatar profile image' do
+    author = feed_items(:dexter_creates_ghost).author
+    result = feed_author_image author
+    
+    assert_match(/<img .*src=".*gravatar\.com.*"/, result)
+    assert_match Digest::MD5.hexdigest(author.display_email), result,
+        'image tag does not contain profile e-mail hash'
+  end
+end
