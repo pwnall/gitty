a31220c8999e6c8657e903f9a973cd3c2b92b17f
Victor Costan <costan@gmail.com>
2011-03-21 01:27:36 -0400

    Updated config_vars.

--- app/controllers/config_vars_controller.rb
+++ app/controllers/config_vars_controller.rb
@@ -1,16 +1,6 @@
 # Viewing and changing configuration variables.
 class ConfigVarsController < ApplicationController
   config_vars_controller
-  
-  # Configuration variables are usually sensitive, so let's put some protection
-  # around modifying them.
-  before_filter :http_basic_check
-  def http_basic_check
-    authenticate_or_request_with_http_basic(
-        ConfigVar['config_vars.http_realm']) do |user, password|
-      user == ConfigVar['config_vars.http_user'] &&
-          password == ConfigVar['config_vars.http_password']
-    end
-  end
-  private :http_basic_check
+
+  config_vars_auth
 end
