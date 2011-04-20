b8ff8ea20a1be1a5c9cf6b346742ba29ebbd90fa
Victor Costan <costan@gmail.com>
2011-04-20 16:02:50 -0400

    Updated to Rails 3.0.7.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110401233543) do
+ActiveRecord::Schema.define(:version => 20101212042214) do
 
   create_table "acl_entries", :force => true do |t|
     t.string   "role",           :null => false
