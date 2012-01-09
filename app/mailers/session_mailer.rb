893db6c6c4f6626229788c2653b3192b322fd282
Victor Costan <costan@gmail.com>
2012-01-09 00:13:27 -0500

    Integrated revised authpwn password reset API.

--- app/mailers/session_mailer.rb
+++ app/mailers/session_mailer.rb
@@ -1,15 +1,15 @@
 class SessionMailer < ActionMailer::Base
   include Authpwn::SessionMailer
 
-  # The subject line in a password reset e-mail.
-  def reset_password_subject(token, root_url)
-    "#{root_url} password reset"
+  def reset_password_subject(token, server_hostname)
+    # Consider replacing the hostname with a user-friendly application name.
+    "#{server_hostname} password reset"
   end
   
   # The sender e-mail address for a password reset e-mail.
-  def reset_password_from(token, root_url)
-    # You must replace the e-mail address below.
-    "#{root_url} staff <costan@mit.edu>"
+  def reset_password_from(token, server_hostname)
+    # You most likely need to replace the e-mail address below.
+    "#{server_hostname} staff <admin@#{server_hostname}>"
   end
 
   # Add your extensions to the SessionMailer class here.  
--- test/functional/session_mailer_test.rb
+++ test/functional/session_mailer_test.rb
@@ -4,18 +4,19 @@ class SessionMailerTest < ActionMailer::TestCase
   setup do
     @email = credentials(:jane_email).email
     @reset_token = credentials(:jane_password_token)
-    @root_url = 'http://site.com'
-    @token_url = "#{@root_url}/tokens/#{@reset_token.code}"
+    @host = 'test.host'
   end
 
   test 'password_reset email' do
-    email = SessionMailer.reset_password_email(@email, @reset_token, @root_url,
-        @token_url).deliver
+    email = SessionMailer.reset_password_email(@email, @reset_token, @host).
+                          deliver
     assert !ActionMailer::Base.deliveries.empty?
     
-    assert_equal 'http://site.com password reset', email.subject
-    assert_equal 'http://site.com staff <costan@mit.edu>', email.from
+    assert_equal 'test.host password reset', email.subject
+    assert_equal ['admin@test.host'], email.from
+    assert_equal '"test.host staff" <admin@test.host>', email['from'].to_s
     assert_equal [@email], email.to
-    assert_match @token_url, email.encoded
+    assert_match @reset_token.code, email.encoded
+    assert_match @host, email.encoded
   end  
 end
