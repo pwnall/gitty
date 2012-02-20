3343cff3eaa9018827a125255204db55cd33d26a
Victor Costan <costan@gmail.com>
2012-02-20 18:47:57 -0500

    Fedora fixes (SElinux and MRI 1.8 support in git-shell).

--- script/git_shell.rb
+++ script/git_shell.rb
@@ -3,8 +3,13 @@
 # This is invoked by sshd when the git user logs in.
 
 require 'net/http'
-require 'rubygems'
-require 'json'
+begin
+  require 'json'
+rescue LoadError
+  # Ruby 1.8
+  require 'rubygems'
+  require 'json'
+end
 
 load File.expand_path('../../lib/git_shell_executor.rb', __FILE__)
 GitShellExecutor.new.run ARGV
