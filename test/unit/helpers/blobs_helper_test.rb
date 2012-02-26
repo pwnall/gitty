3693c636a72c5bf9bae7fb0be8fc8c7cc7b80d4d
Victor Costan <costan@gmail.com>
2012-02-25 19:32:42 -0500

    Unit tests for marked_up_blob.

--- test/unit/helpers/blobs_helper_test.rb
+++ test/unit/helpers/blobs_helper_test.rb
@@ -4,6 +4,7 @@ class BlobsHelperTest < ActionView::TestCase
   setup do
     @branch = branches(:master)
     @commit = commits(:commit1)
+    @blob = blobs(:d1_d2_a)
     @gid = @commit.gitid
   end
   
@@ -19,5 +20,20 @@ class BlobsHelperTest < ActionView::TestCase
                  blob_path(@commit, 'd1/d2/a')
     assert_equal "/dexter/ghost/raw/#{@gid}/d1/d2/a",
                  raw_blob_path(@commit, 'd1/d2/a')
+  end
+  
+  test 'raw_blob_path with branch' do
+    assert_equal '/dexter/ghost/blob/master/d1/d2/a',
+                 blob_path(@branch, '/d1/d2/a')
+    assert_equal '/dexter/ghost/raw/master/d1/d2/a',
+                 raw_blob_path(@branch, '/d1/d2/a')
+  end
+
+  test 'marked_up_blob' do
+    mock_repository_path @blob.repository
+    html = marked_up_blob @blob, '/d1/d2/a'
+    assert html.html_safe?, 'output not marked as html_safe'
+    assert_match /<div class="markdpwn-parsed-code">/, html
+    assert_match "Version 1", html
   end
 end
