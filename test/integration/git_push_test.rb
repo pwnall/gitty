d41632fb8b5b51774670807333b996483a18dadd
Victor Costan <costan@gmail.com>
2012-02-27 04:57:01 -0500

    Extend server startup limit for integration test.

--- test/integration/git_push_test.rb
+++ test/integration/git_push_test.rb
@@ -49,7 +49,7 @@ END_SHELL
     @fixture_repo_path = Rails.root.join 'test', 'fixtures', 'repo.git'
 
     # Wait until the Rails server has booted.
-    deadline = Time.now + 10.seconds
+    deadline = Time.now + 30.seconds
     loop do
       begin
         Net::HTTP.get URI.parse(ConfigVar['app_uri'])
